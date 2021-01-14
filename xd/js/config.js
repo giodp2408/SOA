b7a5013d349f95fddc5117b5a43d8bfba48a4db2
Jesuscua <60193941+Jesuscua@users.noreply.github.com>
2021-01-14 00:34:37 -0600

    Actualizacion

---
+++ xd/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_P8pkzQnu0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1hcrbrbp9i1rb20b0ghcjdoi4u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
